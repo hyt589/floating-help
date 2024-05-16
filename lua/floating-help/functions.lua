843a1c199a62a36673a9a7d54018cc6c12096584
Yutong Huang <yutong.huang@outlook.com>
2024-05-16 23:07:49 +0800

    feat: enable window width configuration

--- lua/floating-help/functions.lua
+++ lua/floating-help/functions.lua
@@ -1,41 +1,43 @@
-local Util     = require('floating-help.util')
-local Options  = require('floating-help.options')
+local Util = require("floating-help.util")
+local Options = require("floating-help.options")
 
-local M        = {}
+local M = {}
 
-M.initialize   = function(opts)
-  if type(opts) == 'table' then
-    for key, value in pairs(opts) do
-      if Options.args[key] ~= nil then
-        Options.args[key] = Util.normalize(key, value)
-      end
+M.initialize = function(opts)
+	if type(opts) == "table" then
+		for key, value in pairs(opts) do
+			if Options.args[key] ~= nil then
+				Options.args[key] = Util.normalize(key, value)
+			end
 
-      if Options.float_opts.window_opts[key] ~= nil then
-        Options.float_opts.window_opts[key] = Util.normalize(key, value)
-      end
-    end
-  end
+			Options.float_opts.width = Options.args.width
+
+			if Options.float_opts.window_opts[key] ~= nil then
+				Options.float_opts.window_opts[key] = Util.normalize(key, value)
+			end
+		end
+	end
 end
 
 M.create_group = function()
-  if vim.fn.exists('#' .. Options.vars.group_name) == 1 then
-    vim.api.nvim_del_augroup_by_name(Options.vars.group_name)
-  end
+	if vim.fn.exists("#" .. Options.vars.group_name) == 1 then
+		vim.api.nvim_del_augroup_by_name(Options.vars.group_name)
+	end
 
-  return vim.api.nvim_create_augroup(Options.vars.group_name, { clear = true })
+	return vim.api.nvim_create_augroup(Options.vars.group_name, { clear = true })
 end
 
-M.open_help    = function(buf)
-  if buf ~= nil and vim.bo[buf].filetype == 'help' then
-    local current_help_win = vim.api.nvim_get_current_win()
-    local help_win = Util.create_help_window(buf, Options)
+M.open_help = function(buf)
+	if buf ~= nil and vim.bo[buf].filetype == "help" then
+		local current_help_win = vim.api.nvim_get_current_win()
+		local help_win = Util.create_help_window(buf, Options)
 
-    -- Set help scroll position
-    vim.wo[help_win].scroll = vim.wo[current_help_win].scroll
+		-- Set help scroll position
+		vim.wo[help_win].scroll = vim.wo[current_help_win].scroll
 
-    -- Close common help window
-    vim.api.nvim_win_close(current_help_win, false)
-  end
+		-- Close common help window
+		vim.api.nvim_win_close(current_help_win, false)
+	end
 end
 
 return M
--- lua/floating-help/options.lua
+++ lua/floating-help/options.lua
@@ -1,29 +1,30 @@
-local M      = {}
+local M = {}
 
-M.args       = {
-  border = 'solid', -- none, single, double, rounded, solid, shadow, array
-  ratio = 0.8,
+M.args = {
+	border = "solid", -- none, single, double, rounded, solid, shadow, array
+	ratio = 0.8,
+	width = 78,
 }
 
-M.vars       = {
-  group_name = 'FloatingHelpPlugin'
+M.vars = {
+	group_name = "FloatingHelpPlugin",
 }
 
 M.float_opts = {
-  width = 78,
-  height = 0,
-  window_opts = {
-    border = M.args.border,
-    style = 'minimal',
-    anchor = 'NW',
-    relative = 'editor',
-    offset_x = 0,
-    offset_y = 0,
-    zindex = 49,
-    external = false,
-    focusable = true,
-    noautocmd = true,
-  },
+	width = 78,
+	height = 0,
+	window_opts = {
+		border = M.args.border,
+		style = "minimal",
+		anchor = "NW",
+		relative = "editor",
+		offset_x = 0,
+		offset_y = 0,
+		zindex = 49,
+		external = false,
+		focusable = true,
+		noautocmd = true,
+	},
 }
 
 return M
